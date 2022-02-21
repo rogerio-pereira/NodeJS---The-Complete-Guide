c1a0f25137c82f54421cb42ef37993bb0bd14b30
Rogerio Pereira <rpereira@acslabtest.com>
2022-02-21 17:10:47 -0500

    38  - Using Node module system

--- Session 3 - Node basics/app.js
+++ Session 3 - Node basics/app.js
@@ -1,44 +1,9 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((request, response) => {
-    const url = request.url;
-    const method = request.method;
+// const server = http.createServer(routes); //When routes is a simple object
+const server = http.createServer(routes.handler); //When routes is a complex object
 
-    if(url === '/') {
-        response.write('<html>');
-        response.write('<head><title>Enter Message</title></head>');
-        response.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button>Send</button></form></body>');
-        response.write('</html>');
-        return response.end();  //Return will make sure the code stops here
-    }
-
-    if(url === '/message' && method === 'POST') {
-        const body = [];
-
-        request.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        })
-
-        return request.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (error) => { 
-                response.statusCode = 302;
-                response.setHeader('Location', '/')
-                return response.end();
-            });   //Write file async
-        })
-    }
-
-    response.setHeader('Content-Type', 'text-html');
-    response.write('<html>');
-    response.write('<head><title>NEW NODE SERVER</title></head>');
-    response.write('<body><h1>First Page</h1></body>');
-    response.write('</html>');
-    response.end();
-    
-});
+console.log(routes.someText)    //Accessing complex object property
 
 server.listen(3000);
