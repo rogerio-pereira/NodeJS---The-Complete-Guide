16235441254cea0d26648307d455fb7e07d71667
Rogerio Pereira <rpereira@acslabtest.com>
2022-02-21 13:41:21 -0500

    16 -Arrays and arrays methods

--- Session 2/objects.js
+++ Session 2/objects.js
@@ -18,3 +18,14 @@ const person = {
 }
 
 console.log(person.greet())
+
+//Array can have multiple types
+// hoobies = ['Sports', 'Cooking', 1, false, {}];
+hobbies = ['Sports', 'Cooking'];
+for(let hobby of hobbies) { //You cna use let in loops, i thought it only could be constants
+    console.log(hobby)
+}
+
+//MAP will transform the original array and return a new array. It will run the function on each array element
+console.log(hobbies.map(hobby =>  hobby+' (Hobby)' )); //Will add ' (Hobby)' to all array elements
+console.log(hobbies); //print original array (it wasn`t changed because map will return a new array)
