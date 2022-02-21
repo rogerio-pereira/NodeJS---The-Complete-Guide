cad6acaef961b9d864e11363adabbbbc587a8e1f
Rogerio Pereira <rpereira@acslabtest.com>
2022-02-21 15:39:01 -0500

    30 - Sending Response

--- Session 3 - Node basics/app.js
+++ Session 3 - Node basics/app.js
@@ -10,6 +10,13 @@ const server = http.createServer((request, response) => {
     console.log(request.method);
     console.log(request.headers);
     // process.exit(); //This will abort node execution
+
+    response.setHeader('Content-Type', 'text-html');
+    response.write('<html>');
+    response.write('<head><title>NEW NODE SERVER</title></head>');
+    response.write('<body><h1>First Page</h1></body>');
+    response.write('</html>');
+    response.end();
 });
 
 server.listen(3000);
