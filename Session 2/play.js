75a48f635b5bbd5e052c238987b7fad96bdb69b2
Rogerio Pereira <rpereira@acslabtest.com>
2022-02-21 13:22:45 -0500

    14 - Arrow functions

--- Session 2/play.js
+++ Session 2/play.js
@@ -9,11 +9,39 @@ const name = 'Rogerio';       //String
 // name = 'Rogerio Pereira';   //Will return an error "Assignment to constant variable"
 
 
-function summarizeUser(userName, userAge, userHasHobbies) {
+// function summarizeUser(userName, userAge, userHasHobbies) {
+//     return 'Name is '+userName+
+//             ', age is '+userAge+
+//             ', user has hobbies '+userHasHobbies
+// }
+
+//Rewriting code above assigning function to a variable
+// const summarizeUser = function (userName, userAge, userHasHobbies) {
+//     return 'Name is '+userName+
+//             ', age is '+userAge+
+//             ', user has hobbies '+userHasHobbies
+// }
+//this way you variable summarizeUser will contain the funcion
+//This seems weird to me, i can`t imagine a use case for that
+
+//Rewriting code above using arrow function
+const summarizeUser = (userName, userAge, userHasHobbies) => {
     return 'Name is '+userName+
             ', age is '+userAge+
             ', user has hobbies '+userHasHobbies
 }
+//In arrow functions, when using `this` it refers to parent`s context
 
+//If arrow functions only has one statement you can remove the braces and keywork return
+const sum = (a, b) => a + b; 
+
+//If you have only one argument you can remove the parenthesis
+const sumOne = a => a + 1;
+
+//If you have a function without arguments, you need to have a empty parenthesis
+const printName = () => console.log('Rogerio Pereira'); 
 
 console.log(summarizeUser(name, age, hasHobbies));
+console.log(sum(1,3));
+console.log(sumOne(6));
+console.log(printName());
