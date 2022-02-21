292e90c1e91a6fb2f20a4ff9c0875f6fcf2bbebe
Rogerio Pereira <rpereira@acslabtest.com>
2022-02-21 14:35:30 -0500

    Template literals

---
+++ Session 2/template-literals.js
@@ -0,0 +1,12 @@
+//Different way to write strings
+let a = 'A string'
+let b = "B String"
+let c = `Another way of using strings`
+
+//Third way you can concatenate variables to it
+const name = 'Rogerio Pereira';
+const age = 30;
+console.log(`My name is ${name} and my age is ${age} years old.`);
+
+//way better than
+console.log('My name is '+name+' and my age is '+age+' years old.');
