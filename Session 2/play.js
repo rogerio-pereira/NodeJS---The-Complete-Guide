373788ccb5c21ba0625b99ba1487ec7634f7e304
Rogerio Pereira <rpereira@acslabtest.com>
2022-02-21 13:07:34 -0500

    Let and const

--- Session 2/play.js
+++ Session 2/play.js
@@ -1,7 +1,13 @@
 //Variables
-var name = 'Rogerio';       //String
-var age = 30;               //Number
-var hasHobbies = true;      //Boolean
+//You can create variables using let, var and let are the same thing
+let age = 30;               //Number
+let hasHobbies = true;      //Boolean
+
+//Const never change, that`s why we use const in loops, because that value will never change
+const name = 'Rogerio';       //String
+
+// name = 'Rogerio Pereira';   //Will return an error "Assignment to constant variable"
+
 
 function summarizeUser(userName, userAge, userHasHobbies) {
     return 'Name is '+userName+
