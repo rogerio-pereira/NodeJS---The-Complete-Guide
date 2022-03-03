fdc668bb4f5c77f35822fad940ae78ebb10f497e
Rogerio Pereira <rpereira@acslabtest.com>
2022-03-03 16:08:33 -0500

    64 - Parsing income requets

--- Session 5 - Working with Express-js/app.js
+++ Session 5 - Working with Express-js/app.js
@@ -1,5 +1,6 @@
 // const http = require('http'); // This line can be removed because we`re not using http anymore, we`re using express
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -11,20 +12,29 @@ const app = express();
 // })
 
 //This middleware will be executed for following middlewares because it calls next()
-app.use('/', (req, res, next) => {
-    console.log('this always runs')
-    next();
-})
+// app.use('/', (req, res, next) => {
+//     console.log('this always runs')
+//     next();
+// });
+
+//Register a middleware, this middleware will call next in the end
+//This will parse html bodies
+app.use(bodyParser.urlencoded({extended: false}));  //extended is required, so it will avoid a warning
 
 //This middleware will execute, return the response and second middleware will never be executed, that`s why this should be prior to the next one
 app.use('/add-product', (request, response, next) => {
-    console.log('In another the middleware!');
-    response.send('<h1>Add Product</h1>');
-})
+    response.send("<form action='/product' method='POST'><input type='text' name='title' /> <button type='submit' >Add Product</button></form>");
+});
+app.use('/product', (req, res, next) =>  {
+    // console.log(req.body)
+    console.log(req.body.title)
+    res.redirect('/');
+});
+
+
 app.use('/', (request, response, next) => {
-    console.log('In another the middleware!');
     response.send('<h1>Hello from Express</h1>');
-})
+});
 
 // const server = http.createServer(app);
 // server.listen(3000);
