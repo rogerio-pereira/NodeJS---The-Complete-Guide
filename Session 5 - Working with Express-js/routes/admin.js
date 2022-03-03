c53b57460ca99a33642ce320c65a511860856932
Rogerio Pereira <rpereira@acslabtest.com>
2022-03-03 16:48:20 -0500

    70 - Serving HTML files

--- Session 5 - Working with Express-js/routes/admin.js
+++ Session 5 - Working with Express-js/routes/admin.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express')
 
 const router = express.Router();
 
 router.get('/add-product', (request, response, next) => {
-    response.send("<form action='/admin/product' method='POST'><input type='text' name='title' /> <button type='submit' >Add Product</button></form>");
+    response.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 router.post('/product', (req, res, next) =>  { 
     console.log(req.body)
--- Session 5 - Working with Express-js/routes/shop.js
+++ Session 5 - Working with Express-js/routes/shop.js
@@ -1,9 +1,12 @@
+const path = require('path');   //Will allow using relative paths
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (request, response, next) => {
-    response.send('<h1>Hello from Express</h1>');
+    //path.join - will create a path, joining different segments based on current operational system
+    //__dirname means current folder
+    response.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
