047a092d8f779d2af92f8d09849ac5d1ddcb18b5
Rogerio Pereira <rpereira@acslabtest.com>
2022-03-03 16:27:55 -0500

    67 - Adding a 404 page

--- Session 5 - Working with Express-js/app.js
+++ Session 5 - Working with Express-js/app.js
@@ -16,5 +16,11 @@ app.use(bodyParser.urlencoded({extended: false}));  //extended is required, so i
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+//Middleware for Error pages
+//.use will work as a catchall, for all http methods (get, post, put, patch, delete)
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not Found!</h1>')
+})
+
 //Start server and listen to port 3000
 app.listen(3000);
