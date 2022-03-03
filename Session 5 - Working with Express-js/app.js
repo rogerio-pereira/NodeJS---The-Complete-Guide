2536d6ced06956a605a19f751fcc9169a455e992
Rogerio Pereira <rpereira@acslabtest.com>
2022-03-03 15:54:55 -0500

    63 - Handing different routes

--- Session 5 - Working with Express-js/app.js
+++ Session 5 - Working with Express-js/app.js
@@ -5,11 +5,23 @@ const app = express();
 
 //Next is a function that will allow chaining middlewares
 //If you want to stop to request you can return a response, instead of calling next()
-app.use((request, response, next) => {
-    console.log('In the middleware!');
-    next(); //Allows the request to continue to next middleware in line
+// app.use((request, response, next) => {
+//     console.log('In the middleware!');
+//     next(); //Allows the request to continue to next middleware in line
+// })
+
+//This middleware will be executed for following middlewares because it calls next()
+app.use('/', (req, res, next) => {
+    console.log('this always runs')
+    next();
 })
-app.use((request, response, next) => {
+
+//This middleware will execute, return the response and second middleware will never be executed, that`s why this should be prior to the next one
+app.use('/add-product', (request, response, next) => {
+    console.log('In another the middleware!');
+    response.send('<h1>Add Product</h1>');
+})
+app.use('/', (request, response, next) => {
     console.log('In another the middleware!');
     response.send('<h1>Hello from Express</h1>');
 })
