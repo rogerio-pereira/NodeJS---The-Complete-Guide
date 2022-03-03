cb696a0be1fedaa78541b78c3327be7c5b8e7ce9
Rogerio Pereira <rpereira@acslabtest.com>
2022-03-03 15:38:59 -0500

    60 - Adding a middleware

--- Session 5 - Working with Express-js/app.js
+++ Session 5 - Working with Express-js/app.js
@@ -2,6 +2,18 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+
+//Next is a function that will allow chaining middlewares
+//If you want to stop to request you can return a response, instead of calling next()
+app.use((request, response, next) => {
+    console.log('In the middleware!');
+    next(); //Allows the request to continue to next middleware in line
+})
+app.use((request, response, next) => {
+    console.log('In another the middleware!');
+    //...
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
